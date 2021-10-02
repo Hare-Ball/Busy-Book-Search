4cd6544a35951ec0fa56f09239a885d91fb8fb27
Hare-Ball <robby.hare@gmail.com>
2021-10-01 19:32:45 -0500

    updates from tutoring

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express')
 const db = require('./config/connection');
-const routes = require('./routes');
+//const routes = require('./routes');
+const { typeDefs, resolvers }
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +16,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//app.use(routes);
+app.get('*', (req, res)=> {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+});
 });
